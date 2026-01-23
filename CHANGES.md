7fa8036c47d447e6d5a80597fd8304e808e960b5
Sebastian Mieruch <sebastian.mieruch@awi.de>
2026-01-23 13:16:17 +0100

    adaption to full dataset

--- CHANGES.md
+++ CHANGES.md
@@ -1,5 +1,10 @@
 # CHANGES
 
+## 2026/01/23
+
+- Adaption to full dataset, i.e. read 3 sheets = 3 time series 
+
+
 ## 2026/01/14
 
 - Initialization.
--- OpenSeaData2ODV.py
+++ OpenSeaData2ODV.py
@@ -20,14 +20,51 @@ pd.set_option("display.width", None)
 
 
 ###-------------------set parameters-------------------###
-DATA_DIR = Path("/home/smieruch/Projects/Helgoland")
+DATA_DIR = Path("/home/smieruch/Projects/Helgoland/Full")
+INPUT_XLSX = "Abiotics ODV.xlsx"
+SHEET_NAMES = ["Abiotics Sea", "Abiotics Pool", "Abiotics Harbour"]
 HEADER_FILE = DATA_DIR / "helgoland_odv_header.txt"
 CRUISE_NAME = "Helgoland_OpenSea"
-STATION_NAME = "Felswatt"
+#STATION_NAME = "Felswatt" -> we will use the SHEET_NAMES
 TYPE_NAME = "B"
-OUTFILE = DATA_DIR / f"{CRUISE_NAME}_{STATION_NAME}.txt"
+OUTFILE = DATA_DIR / f"{CRUISE_NAME}.txt"
+###
+###---new meta variables---###
+#INSTRUMENT_TEMPERATURE = ""
+#INSTRUMENT_SALINITY = ""
+#REPORT = "report.pdf"
 ###----------------------------------------------------###
 
+
+
+
+
+###-------------helper functions---------------------------------------###
+def date_to_iso(df: pd.DataFrame, **_) -> pd.Series:
+    ###empty Time values are read as NaN
+    ###replace empty as ""
+    if "Time" in df.columns:
+        xtime = (
+            df["Time"]
+            .where(df["Time"].isna(), "T" + df["Time"].astype(str))
+            .fillna("")
+        )
+    else:
+        xtime = pd.Series("", index=df.index)
+    
+    return (
+        df["Year"].astype(int).astype(str)
+        + "-"
+        + df["Month"].astype(int).astype(str).str.zfill(2)
+        + "-"
+        + df["Day"].astype(int).astype(str).str.zfill(2)
+        + xtime
+    )
+
+
+def add_station_name(df: pd.DataFrame, sheet_name: str, **_) -> str:
+    return sheet_name
+###------------------------------------------------------------------###
 
 
 ###------------------create data model------------------###
@@ -35,6 +72,7 @@ OUTFILE = DATA_DIR / f"{CRUISE_NAME}_{STATION_NAME}.txt"
 SPEC is the data model that can be easily extended.
 Keep in mind to adapt the header file if SPEC is changed!
 3 rules are available:
+- "const" just adds a constant to the output
 - "fn" applies a function to the input, then maps the transformed data to output
 - "ref" copies a column to new output
 - "src" just copies input to output
@@ -42,97 +80,73 @@ Keep in mind to adapt the header file if SPEC is changed!
 We will loop over SPEC keys and apply what is defined in the values (rules).
 """
 SPEC = {
-    "Cruise": {"fn": lambda df, path: CRUISE_NAME},
-    "Station": {"fn": lambda df, path: STATION_NAME},
-    "Type": {"fn": lambda df, path: TYPE_NAME},
-    "yyyy-mm-dd": {"fn": lambda df, path: year_month_to_iso(df)},
-    "xlon": {"fn": lambda df, path: decode_groups3_scaled_to_decimal_series(df["Longitude"])},
-    "xlat": {"fn": lambda df, path: decode_groups3_scaled_to_decimal_series(df["Latitude"])},
-    "time_ISO8601": {"ref": "yyyy-mm-dd"},
+    "Cruise": {"const": CRUISE_NAME},
+    "Station": {"fn": add_station_name},
+    "Type": {"const": TYPE_NAME},
+    "yyyy-mm-ddThh:mm": {"fn": date_to_iso},
+    "xlon": {"src": "Longitude [degrees_east]"},
+    "xlat": {"src": "Latitude [degrees_north]"},
+    "time_ISO8601": {"ref": "yyyy-mm-ddThh:mm"},
+    "Time TNW": {"src": "Time TNW"},    
     #use later in header https://vocab.nerc.ac.uk/collection/P01/current/TEMPP901/
-    "Temperature Ocean [~^o~#C]": {"src": "Temperatur °C (Meer)"},
-    "Temperature Air [~^o~#C]": {"src": "Temperatur °C (Luft)"},
+    "Temperature Sea [~^o~#C]": {"src": "Temperature °C (Sea)"},
+    "Temperature Air [~^o~#C]": {"src": "Temperature °C (Air)"},
     #https://vocab.nerc.ac.uk/collection/P01/current/PHXXZZXX/
     "pH": {"src": "pH-Wert"},
     #https://vocab.nerc.ac.uk/collection/P01/current/PSALZZXX/
-    "Practical Salinity": {"src": "Salinität (‰)"},
-    "Wind Speed [m/s]": {"src": "Windgeschwindigkeit (m/s)"},
-    "Illuminance [lux]": {"src": "Lichtintensität (lux)"},
+    "Practical Salinity": {"src": "Salinity (‰)"},
+    "Wind Speed [m/s]": {"src": "Wind speed (m/s)"},
+    "Illuminance [lux]": {"src": "Light intensity (lux)"},
+    "Secchi depth [m]": {"src": "Secchi depth (m)"},
+    "Euphotic zone (Secchi depth x 2) [m]": {"src": "Euphotic zone (Secchi depth x 2) (m)"},
+    "1/2 Secchi depth [m]": {"src": "1/2 Secchi depth (m)"},
+    "Color of Forel-Ule scale at 1/2 Secchi depth": {"src": "Color of Forel-Ule scale at 1/2 Secchi depth"},
+    "Weather": {"src": "Weather"},
+    "Group": {"src": "Group"},
+    "Comment": {"src": "Comment"},
+    "Measurement instrument": {"src": "Measurement instrument"}
 }
 
 
-###----------------------------------------------------###
-
-
-def year_month_to_iso(df: pd.DataFrame) -> pd.Series:
-    """
-    Build ISO-8601 YYYY-MM strings from Year and Month columns.
-
-    Parameters
-    ----------
-    df : pandas.DataFrame
-        Must contain integer columns 'Year' and 'Month'.
-
-    Returns
-    -------
-    pandas.Series
-        ISO-8601 formatted year-month-day strings, e.g. '2024-03-01'.
-    """
-    return (
-        df["Jahr"].astype(int).astype(str)
-        + "-"
-        + df["Monat"].astype(int).astype(str).str.zfill(2)
-        + "-01"
-    )
-
-
-def decode_groups3_scaled_to_decimal_series(s: pd.Series) -> pd.Series:
-    """
-    Convert Excel-encoded D,MMM,SSS coordinate to decimal degrees.
-    """
-    
-    x = s.astype("Int64")
-    sign = (x < 0).map({True: -1, False: 1})
-    x = x.abs()
-
-    # Excel stores D,MMM,SSS where MMM and SSS are thousandths,
-    #remove last 6 digits
-    deg = x // 1_000_000
-    #remove last 3 digits, take the remainder of modulo 1000, i.e. remove the first digit 
-    mmm = (x // 1_000) % 1_000 
-    # remainder modulo 1000 (modulo=what is left after devision)
-    sss = x % 1_000 
-
-    minutes = mmm * 60.0 / 1000.0
-    seconds = sss * 60.0 / 1000.0
-
-    return sign * (deg + minutes / 60.0 + seconds / 3600.0)
-
-
-def transform_one_file(df_in: pd.DataFrame, path: Path) -> pd.DataFrame:
+###take the xlsx sheet and loop over the SPEC and apply rules -> association xlsx to odv
+def transform_one_file(df_in: pd.DataFrame, sheet_name: str) -> pd.DataFrame:
     out = pd.DataFrame(index=df_in.index)
 
     for out_col, rule in SPEC.items():
         if "fn" in rule:
-            out[out_col] = rule["fn"](df_in, path)
+            out[out_col] = rule["fn"](df_in, sheet_name=sheet_name)
         elif "src" in rule:
             src = rule["src"]
             out[out_col] = df_in[src] if src in df_in.columns else pd.NA
         elif "ref" in rule:
             out[out_col] = out[rule["ref"]]
+        elif "const" in rule:
+            out[out_col] = rule["const"]
         else:
             out[out_col] = pd.NA
     return out
 
 
-
+###write output + header
 def write_odv_with_header(df_out, header_path: Path, outfile: Path) -> None:
     header_text = header_path.read_text(encoding="utf-8")
 
-    # 1) write the header (overwrite any existing output)
+    #pre
+    # df_out.to_csv(
+    #     outfile,
+    #     sep="\t",
+    #     index=False,
+    #     header=True,
+    #     mode="w",
+    #     encoding="utf-8",
+    #     lineterminator="\n",
+    # )
+
+    
+    #1) write the header (overwrite any existing output)
     outfile.write_text(header_text.rstrip("\n") + "\n", encoding="utf-8")
 
-    # 2) append the data table (no header line from pandas)
+    #2) append the data table (no header line from pandas)
     df_out.to_csv(
         outfile,
         sep="\t",
@@ -146,17 +160,18 @@ def write_odv_with_header(df_out, header_path: Path, outfile: Path) -> None:
 
 def main():
     rows = []
-    #glob=find files by pattern
-    for path in DATA_DIR.glob("*.xlsx"):
-        df_in = pd.read_excel(path) 
+    for name in SHEET_NAMES:
+        print(f"proceccing sheet: {name}")
+        df_in = pd.read_excel(DATA_DIR / INPUT_XLSX, sheet_name=name) 
         df_in.columns = df_in.columns.str.strip() # remove space
-        rows.append(transform_one_file(df_in, path))
-
+        rows.append(transform_one_file(df_in, sheet_name=name))
+        #print(df_in.head())
+        
     df_out = pd.concat(rows, ignore_index=True)
 
-    print(df_out.head())
+    #print(df_out.head())
 
-    ###print to disc
+    ###write to disc
     write_odv_with_header(df_out, HEADER_FILE, OUTFILE)
 
 
